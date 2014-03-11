07774dd517738e274f4b6f050db50ca32276fdf5
Leonel Avila <vical.dru@hotmail.com>
2014-03-11 06:46:05 -0600

    Update Conversor_201213255.java

--- Conversor_201213255.java
+++ Conversor_201213255.java
@@ -17,21 +17,15 @@ public class Conversor_201213255 extends JFrame{
 	public JTextField cel2 = new JTextField("0");
 	public JTextField far2 = new JTextField("0");
 
-	public JButton Convertir = new JButton("Convertir");
-
-	public JLabel kel3 = new JLabel("Kelvin:     ");
-	public JLabel cel3 = new JLabel("Celsius:    ");
-	public JLabel far3 = new JLabel("Fahrenheit: ");
-
-	public JLabel kel4 = new JLabel("0");
-	public JLabel cel4 = new JLabel("0");
-	public JLabel far4 = new JLabel("0");
+	public JButton Convertir1 = new JButton("Convertir");
+	public JButton Convertir2 = new JButton("Convertir");
+	public JButton Convertir3 = new JButton("Convertir");
 
 
 	public void Programa(){
-//Propiedades de ventana
-		this.setTitle("201213255");
-		this.setSize(250,300);
+		//Propiedades de ventana
+		this.setTitle("[IPC1B]Tarea4_201213255");
+		this.setSize(400,160);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -43,16 +37,12 @@ public class Conversor_201213255 extends JFrame{
 		kel1.setBounds(20, 40, 80, 20);
 		cel1.setBounds(20, 70, 80, 20);
 		far1.setBounds(20, 100, 80, 20);
-		kel2.setBounds(110, 40, 100, 20);
-		cel2.setBounds(110, 70, 100, 20);
-		far2.setBounds(110, 100, 100, 20);
-		Convertir.setBounds(20, 130, 190, 20);
-		kel3.setBounds(20, 160, 80, 20);
-		cel3.setBounds(20, 190, 80, 20);
-		far3.setBounds(20, 220, 80, 20);
-		kel4.setBounds(110, 160, 100, 20);
-		cel4.setBounds(110, 190, 100, 20);
-		far4.setBounds(110, 220, 100, 20);
+		kel2.setBounds(110, 40, 150, 20);
+		cel2.setBounds(110, 70, 150, 20);
+		far2.setBounds(110, 100, 150, 20);
+		Convertir1.setBounds(270, 40, 100, 20);
+		Convertir2.setBounds(270, 70, 100, 20);
+		Convertir3.setBounds(270, 100, 100, 20);
 
 		//Haciendo variables visibles
 		this.add(info1);
@@ -62,50 +52,62 @@ public class Conversor_201213255 extends JFrame{
 		this.add(kel2);
 		this.add(cel2);
 		this.add(far2);
-		this.add(Convertir);
-		this.add(kel3);
-		this.add(cel3);
-		this.add(far3);
-		this.add(kel4);
-		this.add(cel4);
-		this.add(far4);
-
+		this.add(Convertir1);
+		this.add(Convertir2);
+		this.add(Convertir3);
 	}
 
-	//Accion de boton "Reiniciar"
+	//Accion de boton "Convertir1"
 	{
-		Convertir.addActionListener(new ActionListener(){
+		Convertir1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent evt){
 				Kelvin = Double.parseDouble(kel2.getText());
 				Celsius = Double.parseDouble(cel2.getText());
 				Fahrenheit = Double.parseDouble(far2.getText());
 
-				if(cel2.getText().equals("0") && far2.getText().equals("0")){
 					Numero1 = Kelvin - 273.15;
-					kel4.setText(Kelvin+"");
-					cel4.setText(Numero1+"");
+					cel2.setText(Numero1+"");
 					Numero2 = ((Kelvin-273.15)*por)+32;
-					far4.setText(Numero2+"");
-				}else if(kel2.getText().equals("0") && far2.getText().equals("0")){
+					far2.setText(Numero2+"");
+			}
+		});
+	}
+
+	//Accion de boton "Convertir2"
+	{
+		Convertir2.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				Kelvin = Double.parseDouble(kel2.getText());
+				Celsius = Double.parseDouble(cel2.getText());
+				Fahrenheit = Double.parseDouble(far2.getText());
+
 					Numero1 = Celsius + 273.15;
 					Numero2 = ((Celsius*por)+32);
-					kel4.setText(Numero1+"");
-					cel4.setText(Celsius+"");
-					far4.setText(Numero2+"");
-				}else if(kel2.getText().equals("0") && cel2.getText().equals("0")){
+					kel2.setText(Numero1+"");
+					far2.setText(Numero2+"");
+			}
+		});
+	}
+	
+	//Accion de boton "Convertir3"
+	{
+		Convertir3.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){
+				Kelvin = Double.parseDouble(kel2.getText());
+				Celsius = Double.parseDouble(cel2.getText());
+				Fahrenheit = Double.parseDouble(far2.getText());
+
 					Numero1 = ((Fahrenheit-32)*5)/9;
 					Numero2 = (((Fahrenheit-32)*5)/9)+273.15;
-					kel4.setText(Numero2+"");
-					cel4.setText(Numero1+"");
-					far4.setText(Fahrenheit+"");
-				}
+					kel2.setText(Numero2+"");
+					cel2.setText(Numero1+"");
 			}
 		});
 	}
 
 
 	public static void main (String args[]){
-		new CKF_201213255().Programa();
+		new Conversor_201213255().Programa();
 	}
 
 }
