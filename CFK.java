330ff3294b4359f8eaf1e5acde8c5711dff029e8
cristoforo93 <ccastanedapaiz@gmail.com>
2014-03-14 17:48:40 -0600

    Tarea Finalizada

---
+++ CFK.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Yovanni on 11/03/14.
+ */
+
+public class CFK {
+    public static void main(String[] args){
+        InterfazGrafica in = new InterfazGrafica();
+        in.show();
+    }
+}
