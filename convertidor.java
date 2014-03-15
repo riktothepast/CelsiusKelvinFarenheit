2a07346a7c9bbeecd7c9e763df97c492d7a863a3
dann-alvizures <danialvizures1@hotmail.com>
2014-03-14 18:05:18 -0600

    Tarea_finalizada carne 201313963

---
+++ convertidor.java
@@ -0,0 +1,12 @@
+public class Convertidor {
+
+//By:Daniel Alvizures 
+//201313963
+    public static void main(String[] args) {
+      
+        conversorgrados frame= new conversorgrados();
+        frame.setBounds(500,100,300,300); 
+        frame.setVisible(true);
+    }
+    
+}
